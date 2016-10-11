d99a77f40b45e437aedf30ce84da898d69a122b8
sachin782 <sachinrawat782@gmail.com>
2016-10-11 21:16:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+    if(request.status===200){
+    counter=counter+1;
+    var counter=request.responseText;
+    var span=document.getElementById('count');
+    span.innerHTML=couner.toString();
+    }
+    }
+};
+request.open('GET',http://sachin782.imad.hasura-app.io/counter,true);
+request.send(null);
+};
